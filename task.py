46932e9ac589c9abc3f8094eb5d7dfab5b68c07f
mohamed-darwish <mu.darwish23@gmail.com>
2019-01-15 20:50:12 +0200

    project submission

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ from physics_sim import PhysicsSim
 
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None, 
+    def __init__(self, init_pose=None, init_velocities=None,
         init_angle_velocities=None, runtime=5., target_pos=None):
         """Initialize a Task object.
         Params
@@ -15,7 +15,7 @@ class Task():
             target_pos: target/goal (x,y,z) position for the agent
         """
         # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
         self.action_repeat = 3
 
         self.state_size = self.action_repeat * 6
@@ -24,12 +24,24 @@ class Task():
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
+
+    # def get_reward(self):
+    #     """Uses current pose of sim to return reward."""
+    #     reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+    #     return reward
 
     def get_reward(self):
-        """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
-        return reward
+        """Uses current pose and velocity of sim to return reward."""
+        distance = 5 * self.sim.pose[2] - 2 * np.abs(self.sim.pose[0] + self.sim.pose[1])
+        velocity = 5 * self.sim.v[2] - 2 * np.abs(self.sim.v[0] + self.sim.v[1])
+        reward = distance + velocity
+        if self.sim.pose[2] >= self.target_pos[2]:
+            """Distance is positive, then add a reward that is function in distance"""
+            reward += 5 * (self.sim.pose[2] - self.target_pos[2])
+        else:
+            reward -= 5 * (self.sim.pose[2] - self.target_pos[2])
+        return np.tanh(reward)
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
@@ -37,7 +49,7 @@ class Task():
         pose_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
+            reward += self.get_reward()
             pose_all.append(self.sim.pose)
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
@@ -45,5 +57,5 @@ class Task():
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        state = np.concatenate([self.sim.pose] * self.action_repeat)
         return state
